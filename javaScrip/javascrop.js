dc502b672b8986a26e69bd7a7569f7fbfa00e03d
RamiroCabri <cabriramiro@gmail.com>
2023-04-20 23:58:26 -0300

    funcion codificadora lista

--- javaScrip/javascrop.js
+++ javaScrip/javascrop.js
@@ -2,7 +2,31 @@
 function encriptador(){
     
     var algo = document.getElementById('w3review').value;
-    console.log(algo);
-    document.getElementById("test").innerHTML = algo;
+    
+    var nueva = " "
+    for  ( var  i=0 ; i < algo.length; i++) {
+        var letra = algo.charAt(i)
+            if (letra == "a"){
+                nueva += letra+ "i" 
+            }
+            else if (letra == "i"){
+                nueva += letra+ "mes" 
+            }
+            else if  (letra == "e"){
+                nueva += letra + "nter"   
+            }
+            else if  (letra == "o"){
+                nueva += letra + "ber"
+            }
+            else if  (letra == "u"){
+                nueva += letra + "fat" 
+            }
+            else{
+                nueva += letra 
+            }
+
+        }
+    document.getElementById("test").innerHTML = nueva;
 }
+
 
